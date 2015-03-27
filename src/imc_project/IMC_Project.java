a438bdc8cdb792a74a21c92bacf344d06e8a07bb
41321863 <41321863@099237-P31S204.macklabs.br>
2015-03-27 20:56:38 -0300

    20h49 - add all classes

---
+++ src/imc_project/IMC_Project.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imc_projeto;
+
+/**
+ *
+ * @author 41321863
+ */
+import imc_project.Ex2;
+import javax.swing.JFrame;
+
+//Main - Nome da classe
+public class IMC_Project {
+
+    //Comando principal para execução do programa
+
+    public static void main(String[] args) {
+        //Criação de um objeto ex da classe Ex2
+        Ex2 ex = new Ex2();
+        //posição da tela ao abri-la
+        ex.setLocation(400, 200);
+        //tornando a tela visível
+        ex.setVisible(true);
+    }
+}
+
